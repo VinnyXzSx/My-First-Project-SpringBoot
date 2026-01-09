dd12a877b2cf4f97b57a48401bf26bcc10052325
VinnyXzSx <viniciusruela200@gmail.com>
2026-01-09 19:47:19 -0300

    adicionado o "Hello World";

--- src/main/java/dev/viny/My_First_Project_Spring_Boot/MyFirstProjectSpringBootApplication.java
+++ src/main/java/dev/viny/My_First_Project_Spring_Boot/MyFirstProjectSpringBootApplication.java
@@ -2,12 +2,14 @@ package dev.viny.My_First_Project_Spring_Boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class MyFirstProjectSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyFirstProjectSpringBootApplication.class, args);
+		System.out.println("Hello Worl");
 	}
 
 }
