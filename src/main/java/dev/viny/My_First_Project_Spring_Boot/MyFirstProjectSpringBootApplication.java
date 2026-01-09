a07101e38e80ed1e3e3e384569458bdb45cc95e6
VinnyXzSx <viniciusruela200@gmail.com>
2026-01-09 19:06:52 -0300

    Initial commit

---
+++ src/main/java/dev/viny/My_First_Project_Spring_Boot/MyFirstProjectSpringBootApplication.java
@@ -0,0 +1,13 @@
+package dev.viny.My_First_Project_Spring_Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstProjectSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstProjectSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/viny/My_First_Project_Spring_Boot/MyFirstProjectSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.viny.My_First_Project_Spring_Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstProjectSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
